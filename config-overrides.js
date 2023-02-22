e6d2b0ea3df2f7b98fe92e9069878b99bde1cbf8
Derevo1 <a.derevo86@gmail.com>
2023-02-22 14:48:23 +0200

    Updates

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
-
-module.exports = {
-  webpack: function(config, env){
-    config.resolve.plugins.push(new TsconfigPathsPlugin())
-
-    return config
-  }
-}
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './root/index.css';
-import App from './root/App';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App/>);
