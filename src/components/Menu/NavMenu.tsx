017e3e1683b6a7711bd06e81c2d6d1360a507839
Derevo1 <a.derevo86@gmail.com>
2023-02-23 21:03:22 +0200

    feat: improve sidebar look

--- src/components/Menu/NavMenu.tsx
+++ src/components/Menu/NavMenu.tsx
@@ -1,21 +1,41 @@
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 import styles from './NavMenu.module.css'
 import { BrandLogo } from '../General/BrandLogo'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { routesDefinition } from '../General/AppRoutes'
+import { useState } from 'react'
 
 export function NavMenu(){
+  const [ showNavMenu, setShowNavMenu ] = useState(true)
+  const { pathname: currPath } = useLocation()
+
   return (
-    <div className={styles.container}>
-      <BrandLogo logoWidth='100px' taglineWidth='200px' />
+    <div className={`${styles.container} ${!showNavMenu ? styles.container_hidden : ''}`}>
+      <div 
+        className={`${styles.nav_toggle} ${ !showNavMenu ? styles.nav_toggle_off : '' }`} 
+        onClick={() => setShowNavMenu(!showNavMenu)}
+      >
+        <AiOutlineArrowLeft size='20px' />
+      </div>
+      {showNavMenu 
+        ? <BrandLogo logoWidth='100px' taglineWidth='200px' /> 
+        : <></>
+      }
       <ul className={styles.nav_container}>
         {routesDefinition
           .filter((def) => !def.hidden)
           .map((def) => 
             <li>
-              <div className={styles.nav_item}>
-                {def.icon}
-                <Link to={def.path}>{def.name}</Link>
-              </div>
+              <Link to={def.path} style={{textDecoration: 'none', color: 'black'}}>
+                <div 
+                  className={
+                    `${styles.nav_item} ${currPath === def.path ? styles.nav_item_active : '' }`
+                  }
+                >
+                    {def.icon}
+                    {showNavMenu ? def.name : ''}
+                </div>
+              </Link>
             </li>
           )
         }
