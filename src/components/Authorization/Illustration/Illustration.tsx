eeb3aeaa5bc5d5bbb2065c70356c60c401f4891b
Derevo1 <a.derevo86@gmail.com>
2023-03-22 14:57:59 +0200

    feat: sign-in page animation

--- src/components/Authorization/Illustration/Illustration.tsx
+++ src/components/Authorization/Illustration/Illustration.tsx
@@ -6,20 +6,124 @@ import TeamPresentation from '@assets/images/sign-in/widgetsX-team-presentation.
 import TeamConsultig from '@assets/images/sign-in/widgetsX-team-consulting.svg'
 import { BrandLogo } from "../../General/BrandLogo";
 
-export function Illustration(){
+export function Illustration(){  
+  const getIllustrationImages = (repeat?: number) => {
+    let images = [
+      <div className={styles.illustration_container}>
+        <div className={styles.illustration_container_items}>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamSupport}
+              alt="widgetsx"
+              style={{transform: 'rotate(9deg)', right: '10%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+                src={TeamWeb}
+                alt="widgetsx"
+                style={{transform: 'rotate(9deg)', right: '-29%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamAnalytics}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '-40%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamPresentation}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '-80%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamConsultig}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '10%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamSupport}
+              alt="widgetsx"
+              style={{transform: 'rotate(9deg)', right: '-40%', position: 'absolute'}}
+            />
+          </div>
+        </div>
+        <div className={styles.illustration_container_items} style={{animationName: styles.running_team_reverse}}>
+        <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamSupport}
+              alt="widgetsx"
+              style={{transform: 'rotate(9deg)', right: '10%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+                src={TeamWeb}
+                alt="widgetsx"
+                style={{transform: 'rotate(9deg)', right: '-29%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamAnalytics}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '-40%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamPresentation}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '-80%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamConsultig}
+              alt="widgetsx"
+              className={styles.illustration_container_item}
+              style={{transform: 'rotate(9deg)', right: '10%', position: 'absolute'}}
+            />
+          </div>
+          <div style={{ position: 'relative' }}>
+            <img 
+              src={TeamSupport}
+              alt="widgetsx"
+              style={{transform: 'rotate(9deg)', right: '-40%', position: 'absolute'}}
+            />
+          </div>
+        </div>
+      </div>
+    ]
+
+    if(repeat){
+      images = new Array(repeat)
+        .fill(null)
+        .map((_v, _i) => images)
+        .flat()
+    }
+    
+    return images
+  }
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.welcome_label}>
           <BrandLogo logoWidth="300px" />
         </div>
-        <div className={styles.illustration_container}>
-          <img src={TeamSupport} alt="widgetsx" />
-          <img src={TeamWeb} alt="widgetsx" />
-          <img src={TeamAnalytics} alt="widgetsx" />
-          <img src={TeamConsultig} alt="widgetsx" />
-          <img src={TeamPresentation} alt="widgetsx" />
-        </div>
+        {getIllustrationImages(Math.round(window.innerHeight / 1000))}
       </div>
     </>
   )
